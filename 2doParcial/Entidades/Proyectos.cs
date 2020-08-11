6fa3fb17c8803d72d0facb3ef685fc5cb349e064
Elvis Junior Duarte Valerio <elvis_duarte@ucne.edu.do>
2020-08-10 22:35:53 -0400

    Entidades Listas

---
+++ 2doParcial/Entidades/Proyectos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace _2doParcial.Entidades
+{
+    public class Proyectos
+    {
+        [Key]
+        public int ProyectoId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Descripcion { get; set; }
+        public double TiempoTotal { get; set; }
+
+        [ForeignKey("ProyectoId")]
+        public virtual List<ProyectosDetalle> Detalle { get; set; } = new List<ProyectosDetalle>();
+    }
+}
---
+++ 2doParcial/Entidades/ProyectosDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace _2doParcial.Entidades
+{
+    public class ProyectosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProyectoId { get; set; }
+        public int TareaId { get; set; }
+
+        [ForeignKey("TareaId")]
+        public Tareas Tipo { get; set; } = new Tareas();
+
+        public string Requerimiento { get; set; }
+        public double Tiempo { get; set; }
+    }
+}
